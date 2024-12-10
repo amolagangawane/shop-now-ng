f7bc71286812f19d07f413e839f6ab86bd255ed6
amolagangawane <amolagangawane@gmail.com>
2024-12-10 13:42:35 +0530

    env created

---
+++ src/environments/environement.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.example.com',    // Production API URL
+    featureFlag: false                   // Flag for features in production
+  };
+  
